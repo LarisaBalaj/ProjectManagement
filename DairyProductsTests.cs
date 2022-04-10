2acf887eb770daeb1eaeb8c705a98da370baf011
LarisaBalaj <65074652+LarisaBalaj@users.noreply.github.com>
2022-04-10 09:45:00 +0300

    Add files via upload

---
+++ DairyProductsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Management_Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management_Project.Tests
+{
+    [TestClass()]
+    public class DairyProductsTests
+    {
+        private int actual;
+        [TestMethod()]
+        public void DairyProductsTest()
+        {
+            var expected = 4;
+            var stock = new DairyProducts();
+
+            var actual = stock.dairystock(10,6);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
